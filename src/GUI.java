941c3f0c9e3b6a25105ab939e904bd4c2c7bc589
16syms_g <georgessyms@gmail.com>
2022-07-12 10:09:29 +0100

    gui semi working

--- src/GUI.java
+++ src/GUI.java
@@ -4,21 +4,59 @@ import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.invoke.MethodType;
 
 public class GUI extends JPanel implements ActionListener, DocumentListener {
-    // canvas for other GUI widgets
+
     JButton button1;
     JButton button2;
+    JTextField username;
+    JFrame mainFrame = new JFrame("Main Frame");
+    JPanel menu;
+    JPanel login;
+
     public GUI(int width, int height) {
         System.out.println("SQUENCE: GUI constructor");
         this.setPreferredSize(new Dimension(width, height));
+        //JFrame mainFrame = new JFrame("Main Frame");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-        button1 = new JButton("b1");
-        JTextField username = new JTextField();
+    }
+
+    public void login(int width, int height) {
+
+        JPanel login = new JPanel();
+        login.setBounds(100, 500, 1000, 700);
+
+        username = new JTextField("enter username");
+        username.setBounds(20, 60, 40, 200);
+        username.getDocument().addDocumentListener(this);
+
+        button1 = new JButton("Login");
+        button1.setBounds(20, 60, 40, 200);
+        button1.addActionListener(this);
+
+        button2 = new JButton("Exit");
+        button2.setBounds(120, 0, 100, 40);
+        button2.addActionListener(this);
+
+        login.add(username);
+        login.add(button1);
+        login.add(button2);
+
+        mainFrame.setVisible(true);
+        mainFrame.add(login);
+        login.setVisible(true);
+    }
+
+    public void menu(int width, int height) {
+
+        button1 = new JButton("Login");
+        username = new JTextField("enter username");
+        username.setBounds(20, 69, 40, 200);
+        username.getDocument().addDocumentListener(this);
         username.setBounds(20,60,200,40);
         button1.setBounds(0,0, 100, 40);
-        button2 = new JButton("b2");
+        button2 = new JButton("Exit");
         button2.setBounds(120,0, 100, 40);
         button1.addActionListener(this);
         button2.addActionListener(this);
@@ -26,30 +64,24 @@ public class GUI extends JPanel implements ActionListener, DocumentListener {
         add(button1);
         add(username);
         add(button2);
+        mainFrame.add(this);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+
     }
-
-
-    @Override
-    public void insertUpdate(DocumentEvent e){
-        System.out.println("Insert");
+    @Override public void insertUpdate(DocumentEvent e){System.out.println("Insert");
     }
-
-    @Override
-    public void removeUpdate(DocumentEvent e) {
-        System.out.println("remove");
+    @Override public void removeUpdate(DocumentEvent e) {System.out.println("remove");
     }
-
-    @Override
-    public void changedUpdate(DocumentEvent e) {
-        System.out.println("changed");
+    @Override public void changedUpdate(DocumentEvent e) {System.out.println("changed");
     }
 
     public void actionPerformed(ActionEvent e){
-        if (e.getActionCommand() == "b1"){
-            System.out.println("THE FIRST BUTTON");
-        } else{
-            System.out.println("THE SECOND ONE");
-
+        if (e.getActionCommand() == "Login"){
+        System.out.println("Username = " + username.getText());
+    }
+        else{
+            System.out.println("Password = " + username.getText());
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,17 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        /*
+
         // demo GUI
         System.out.println("SEQUENCE: Program started");
-        JFrame frame = new JFrame("Demo frame");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        GUI myGUI = new GUI(600, 400);
-        frame.add(myGUI);
-        frame.pack();
-        frame.setVisible(true);
-        */
 
-        ClickCounter myClickCounter = new ClickCounter();
-        final var myClickCounter1 = myClickCounter;
+        GUI login = new GUI(1000, 700);
+        //GUI menu = new GUI(1000, 700);
+
+        login.login(1000,500);
+
+        //ClickCounter myClickCounter = new ClickCounter();
+        //final var myClickCounter1 = myClickCounter;
 
     }
 }
